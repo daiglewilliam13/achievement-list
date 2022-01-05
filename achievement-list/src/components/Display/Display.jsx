1051d05f296da5a7bd4f3ef3e68f4592b82ef9a0
William Daigle <daiglewilliam13@gmail.com>
2022-01-05 01:54:51 +0000

    update title & cleanup

--- achievement-list/src/components/Display/Display.jsx
+++ achievement-list/src/components/Display/Display.jsx
@@ -4,12 +4,6 @@ import Achievement from '../Achievement/Achievement.jsx';
 
 const Display = (props) =>{
 	const ach = props.achievements
-	const ach2 = ach.map((action, index)=>{
-		
-		<li>
-		<Achievement achievement={action} />
-		</li>
-	})
 	return(
 	<>
 		{ach.map((ach, index)=>{
